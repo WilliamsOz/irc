ef16a30040321f1fe36788595cb8a796beb66198
Oscar Ozserttas <ozserttas.oscar@gmail.com>
2023-08-22 21:03:54 +0200

    Server in progress, receive string from client doesnt work yet

--- includes/irc.hpp
+++ includes/irc.hpp
@@ -3,12 +3,15 @@
 
 # include <iostream>
 
+# include "server.hpp" // test server
 const int MAX_EVENTS = 10; // test server
 # include <sys/epoll.h> // test server
 # include <fcntl.h> // test server
 # include <unistd.h> // test server
 # include <arpa/inet.h> // test server
 # include <cstring> // test server
+
+# include <stdio.h> // pour utilisation de perror
 
 
 
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -22,7 +22,8 @@ int	server()
 	int retBind = bind(socketfd, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
 	if (retBind == -1)
 	{
-		std::cerr << "Error : Cannot link socket." << std::endl;
+		// std::cerr << "Error : Cannot link socket." << std::endl;
+		perror("Error binding socket");
 		return (1);
 	}
 
@@ -35,7 +36,7 @@ int	server()
 	}
 
 	// creation de l'instance de epoll
-	int epollfd = epoll_create(0);
+	int epollfd = epoll_create1(0);
 	if (epollfd == -1)
 	{
 		std::cerr << "Error : Cannot create instance of epoll." << std::endl;
@@ -56,9 +57,59 @@ int	server()
 		return (1);
 	}
 
-	
+	epoll_event events[MAX_EVENTS];
 
+	while (true)
+	{
+        int numEvents = epoll_wait(epollfd, events, MAX_EVENTS, -1); // attend evenement jusqu'a ce que au moin 1 evenement se produise
+        if (numEvents == -1)
+		{
+            std::cerr << "Error : Unable to wait for events." << std::endl;
+            return (1);
+        }
+        for (int i = 0; i < numEvents; i++)
+		{
+            if (events[i].data.fd == socketfd) // nouvelle connexion en attente
+			{
+                int clientSocket = accept(socketfd, NULL, NULL); // connexion entrante accepté et creation de socket client
+																	// changer deuxieme param pour recup info du client et troisieme pour taille de struct
+                if (clientSocket == -1)
+				{
+                    std::cerr << "Error : Unable to accept new client." << std::endl;
+                    continue;
+                }
+                fcntl(clientSocket, F_SETFL, O_NONBLOCK); // change les attributs de clientSocket
+                event.events = EPOLLIN | EPOLLET; // EPOLLET : notifie uniquement lorsque etat du socket change
+                event.data.fd = clientSocket;
+                if (epoll_ctl(epollfd, EPOLL_CTL_ADD, clientSocket, &event) == -1)
+				{
+					std::cerr << "Error : Cannot add client socket in epoll group." << std::endl;
+                    return (1);
+                }
+                std::cout << "New client connected." << std::endl;
+            }
+			else // client deja connecte qui envoi des données
+			{
+                char buffer[1024];
+                int bytesRead = recv(events[i].data.fd, buffer, sizeof(buffer), 0); // read depuis fd du client
+                if (bytesRead <= 0)
+				{
+                    close(events[i].data.fd);
+                    epoll_ctl(epollfd, EPOLL_CTL_DEL, events[i].data.fd, &event);
+                    std::cout << "Client disconnected." << std::endl;
+                }
+				else
+				{
+                    buffer[bytesRead] = '\0';
+					if (strstr(buffer, "/join") != NULL)
+						std::cout << "Commande join recus" << std::endl;
+                }
+            }
+        }
+    }
 
+    close(socketfd);
+    close(epollfd);
 
 	return (0);
 }
